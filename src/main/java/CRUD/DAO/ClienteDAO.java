4bea1899aaf1c1cb19a3288986e3df39f840d32a
Carlos89-web <79230085+Carlos89-web@users.noreply.github.com>
2022-02-01 19:12:54 +0100

    Update ClienteDAO.java

--- src/main/java/CRUD/DAO/ClienteDAO.java
+++ src/main/java/CRUD/DAO/ClienteDAO.java
@@ -4,25 +4,19 @@ import java.util.List;
 
 import CRUD.Controlador.entity.Cliente;
 
-// --- VIDEO 69, 71, 73, 75 ---
-
 public interface ClienteDAO {
 
-	// Declaramos el método que me va a devolver los clientes.
-	// Al crear esta interfaz, me va a permitir hacer una inyección de dependencias allí donde lo 
+	// Declaramos el mÃ©todo que me va a devolver los clientes.
+	// Al crear esta interfaz, me va a permitir hacer una inyecciÃ³n de dependencias allÃ­ donde lo 
 	// necesite del objeto ClienteDAO
 	
 	public List<Cliente> getClientes();
 	
-	// Video 71: Creamos método para insertar clientes en la tabla de la BD
+	// Creamos mÃ©todo para insertar clientes en la tabla de la BD
 
 	public void insertarCliente(Cliente elCliente);
-	
-	// Video 73
 
 	public Cliente getCliente(int id);
-	
-	// Video 75
 
 	public void eliminarCliente(int id);
 
